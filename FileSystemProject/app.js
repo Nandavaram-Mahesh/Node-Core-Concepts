a320e89145a364bdfa82c28b7f78ab69b3c9fafb
Nandavaram-Mahesh <mahesh.nandavaram96@gmail.com>
2024-02-27 21:09:01 +0530

    changes

--- FileSystemProject/app.js
+++ FileSystemProject/app.js
@@ -1,46 +1,82 @@
 const { watch } = require('node:fs/promises');
 const fs = require('node:fs/promises')
 
-async function fileWatch(){
-    try{
+async function fileWatch() {
+    async function createFile(path) {
         
+        try{
+
+            //  Try to open the file using the path
+            let existingFile = fs.open(path,'r');
+            // if the file doesn't exists an error is thrown 
+          
+            return console.log("fileExists")
+
+        }catch(e){
+            let newFile = await fs.open(path,"w")
+            newFile.close()
+            
+        }
+         existingFile.close()
+
+    }
+    try {
+
         // for watching the changes in the file
-        const watcher  =watch('./')
+        const watcher = watch('./')
 
         // for opening the file
-        const fileHandler = await fs.open('file.txt',"r")
+        const fileHandler = await fs.open('file.txt', "r")
 
         //  Custom eventEmitter (All <FileHandle> objects are <EventEmitter>s. it means they extend evenEmitter)
 
-        fileHandler.on("change",async()=>{
-            
-                // getting the size of the file (i.e no of bytes)
-                const fileSize = (await fileHandler.stat()).size
-                
-                // allocating the buffer 
-                const buff = Buffer.alloc(fileSize)
-                
-                // the location at which we want to start filling our buffer from 
-                const offset = 0
-                
-                // length of the buffer in bytes
-                const length = buff.byteLength
-                
-                // the location at which we want to start reading the file from
-                const position = 0
-                
-                // Finally Reading a file
-                const content = await fileHandler.read(buff,offset,length,position)
+        fileHandler.on("change", async () => {
 
-                console.log(content)
+            // getting the size of the file (i.e no of bytes)
+            const fileSize = (await fileHandler.stat()).size
+
+            // allocating the buffer 
+            const buff = Buffer.alloc(fileSize)
+
+            // the location at which we want to start filling our buffer from 
+            const offset = 0
+
+            // length of the buffer in bytes
+            const length = buff.byteLength
+
+            // the location at which we want to start reading the file from
+            const position = 0
+
+            // Finally Reading a file
+            await fileHandler.read(buff, offset, length, position)
+
+
+            /*
+                decoder => takes 0's, 1's and converts them to something meaningful (like characters, image, video...)
+                encoder => it takes something meaningful and converts them into 0's, 1's.
+             */
+
+            const command = buff.toString('utf-8')
+
+            // read the command 
+            // create a file <path>
+            const filePathStartIndex = command.indexOf("./");
+            const filePathEndIndex = command.length;
+            
+            if (command.includes("create a file")) {
+                const filePath = command.substring(filePathStartIndex, filePathEndIndex);
+                createFile(filePath)
+                
+            }
+
         })
 
 
-        for await (const event of watcher){     /* watcher function returns a asyncIterator */
-             
+        for await (const event of watcher) {     /* watcher function returns a asyncIterator */
+
             console.log(event)  /* outPut: { eventType: 'change', filename: 'file.txt' } */
-            
-            if(event.eventType ==="change" && event.filename==='file.txt'){
+
+            if (event.eventType === "change" && event.filename === 'file.txt') {
 
                 /*  
                    File change is detected so next i want to read the content of the file 
@@ -54,7 +90,7 @@ async function fileWatch(){
 
             }
         }
-    }catch(error){
+    } catch (error) {
         console.log(error)
     }
 }
