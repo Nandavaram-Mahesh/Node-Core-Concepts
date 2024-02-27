3759a05e09ae2f4714efe08c5de0b42f73cdcc37
Nandavaram-Mahesh <mahesh.nandavaram96@gmail.com>
2024-02-27 19:45:03 +0530

    added eventEmitter functionality.

--- FileSystemProject/app.js
+++ FileSystemProject/app.js
@@ -10,11 +10,38 @@ async function fileWatch(){
         // for opening the file
         const fileHandler = await fs.open('file.txt',"r")
 
-        //  watcher function returns a asyncIterator 
-        for await (const event of watcher){
+        //  Custom eventEmitter (All <FileHandle> objects are <EventEmitter>s. it means they extend evenEmitter)
+
+        fileHandler.on("change",async()=>{
+            
+                // getting the size of the file (i.e no of bytes)
+                const fileSize = (await fileHandler.stat()).size
+                
+                // allocating the buffer 
+                const buff = Buffer.alloc(fileSize)
+                
+                // the location at which we want to start filling our buffer from 
+                const offset = 0
+                
+                // length of the buffer in bytes
+                const length = buff.byteLength
+                
+                // the location at which we want to start reading the file from
+                const position = 0
+                
+                // Finally Reading a file
+                const content = await fileHandler.read(buff,offset,length,position)
+
+                console.log(content)
+        })
+
+
+        for await (const event of watcher){     /* watcher function returns a asyncIterator */
+             
             console.log(event)  /* outPut: { eventType: 'change', filename: 'file.txt' } */
+            
             if(event.eventType ==="change" && event.filename==='file.txt'){
-                console.log("Change detected in file.txt")
+
                 /*  
                    File change is detected so next i want to read the content of the file 
                    In order to read the file , first file should be opened then we can read/write to the file.
@@ -22,16 +49,8 @@ async function fileWatch(){
                    note: whenever a file is opened its data is not copied into memory , instead an id is given
                    to the file and it is stored in the memory. it is termed as file descriptor 
                 */
-                const fileSize = (await fileHandler.stat()).size
-                const buff = Buffer.alloc(fileSize)
 
-                const offset = 0
-                const length = buff.byteLength
-                const position = 0
-                // Reading a file
-                const content = await fileHandler.read(buff,offset,length,position)
-
-                console.log(content)
+                fileHandler.emit("change")
 
             }
         }
