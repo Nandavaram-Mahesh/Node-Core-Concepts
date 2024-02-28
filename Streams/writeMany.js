e855db8052597d624b9d2cf134b5ff441cc1e4b2
Nandavaram-Mahesh <mahesh.nandavaram96@gmail.com>
2024-02-28 19:27:39 +0530

    writingFiles using callback methods and allocating buffer , this is synchronous method

--- Streams/writeMany.js
+++ Streams/writeMany.js
@@ -1,10 +1,12 @@
-const fs = require("node:fs/promises");
 
-/*
+
+/* Promise Based FileWriting
 Execution Time: 8s
 CPU Usage: 100% (one core)
 Memory Usage: 50MB
-*/
+
+const fs = require("node:fs/promises");
+
 (async () => {
   console.time("writeMany");
   const fileHandle = await fs.open("test.txt", "w");
@@ -14,5 +16,25 @@ Memory Usage: 50MB
   }
   console.timeEnd("writeMany");
 })();
+*/
 
+
+/*
+Execution Time: 1.8s
+CPU Usage: 100% (one core)
+Memory Usage: 50MB
+*/
+const fs = require("node:fs");
+
+(async () => {
+  console.time("writeMany");
+  fs.open("test.txt", "w", (err, fd) => {
+    for (let i = 0; i < 1000000; i++) {
+      const buff = Buffer.from(` ${i} `, "utf-8");
+      fs.writeSync(fd, buff);
+    }
+
+    console.timeEnd("writeMany");
+  });
+})();
 
