83f06f3fb979b57916f7c1c4dcc80e925793862e
Nandavaram-Mahesh <mahesh.nandavaram96@gmail.com>
2024-02-28 19:38:23 +0530

    writing to a file even after the buffer limit is full. This way is  Not advicable

--- Streams/writeMany.js
+++ Streams/writeMany.js
@@ -19,11 +19,11 @@ const fs = require("node:fs/promises");
 */
 
 
-/*
+/*    Synchronous Way of writing to a file using callback
 Execution Time: 1.8s
 CPU Usage: 100% (one core)
 Memory Usage: 50MB
-*/
+
 const fs = require("node:fs");
 
 (async () => {
@@ -37,4 +37,32 @@ const fs = require("node:fs");
     console.timeEnd("writeMany");
   });
 })();
+*/
 
+
+
+/*  Here we are creating a stream and repetitively 
+pushing data to the buffer even after the buffer limit is full.
+This will allot another buffer and occupy the memory.
+
+DON'T DO IT THIS WAY!!!!
+Execution Time: 270ms
+CPU Usage: 100% (one core)
+Memory Usage: 200MB
+
+const fs = require("node:fs/promises");
+
+(async () => {
+  console.time("writeMany");
+  const fileHandle = await fs.open("test.txt", "w");
+
+  const stream = fileHandle.createWriteStream();
+
+  for (let i = 0; i < 1000000; i++) {
+    const buff = Buffer.from(` ${i} `, "utf-8");
+    stream.write(buff);
+  }
+  console.timeEnd("writeMany");
+})();
+
+*/
